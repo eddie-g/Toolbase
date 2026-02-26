c7933ca439df662136c2e64295e6a268e7291fe7
Your Name <your.email@example.com>
2026-02-26 13:57:04 -0600

    Remove black hover overlay from result images

--- resources/views/logo-generator-2.blade.php
+++ resources/views/logo-generator-2.blade.php
@@ -453,11 +453,6 @@
                                     <!-- Image -->
                                     <div class="aspect-square bg-white dark:bg-gray-100 relative group cursor-pointer" @click="zoomImage(image.url)">
                                         <img :src="image.url" :alt="'Logo ' + (index + 1)" class="w-full h-full object-contain p-4">
-                                        <div class="absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-10 transition-opacity flex items-center justify-center">
-                                            <svg class="w-12 h-12 text-white opacity-0 group-hover:opacity-100 transition-opacity" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0zM10 7v6m3-3H7"/>
-                                            </svg>
-                                        </div>
                                     </div>
 
                                     <!-- Actions -->
